afe042eff78cdd29a7b2e8eb763223c45e07759e
bueenoo <uanderson303@gmail.com>
2025-12-28 08:53:00 -0300

    Add files via upload

--- src/handlers/messageCreate.ts
+++ src/handlers/messageCreate.ts
@@ -1,7 +1,2 @@
-import { Message } from "discord.js";
-import { handleWhitelistThreadMessage } from "../modules/whitelist/handler.js";
-
-export async function handleMessageCreate(message: Message) {
-  // whitelist contínua (thread)
-  await handleWhitelistThreadMessage(message);
-}
+// messageCreate.ts - placeholder recreated
+export async function handleMessageCreate() {}
